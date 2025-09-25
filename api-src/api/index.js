2f62d0cf587b8d9cb9798783ebe794abe0c9bdc4
Abdelhakim <abdelhakim.sahifa@gmail.com>
2025-09-25 20:21:25 +0100

    fix1

--- api-src/api/index.js
+++ api-src/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World' });
